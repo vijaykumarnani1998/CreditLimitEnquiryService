acc3183860839fd6618e9a94d4dadd5c7f623eec
vijayKumar <you@example.com>
2023-08-21 16:55:51 +0530

    Folder Structure Created

---
+++ CreditLimitEnquiryService/src/main/java/com/tesco/enquiry/config/CreditLimitEnquirySwaggerConfig.java
@@ -0,0 +1,13 @@
+/**@CopyRights Reserved to TescoBank pvt ltd. We Should not disclose this information to others
+ * Otherwise Terms & Conditions will apply
+ */
+package com.tesco.enquiry.config;
+
+/**
+ * @author Vijay Kumar at Aug 21, 2023
+ *
+ * 
+ */
+public class CreditLimitEnquirySwaggerConfig {
+
+}
---
+++ CreditLimitEnquiryService/src/main/java/com/tesco/enquiry/service/ICreditLmitEnquiryService.java
@@ -0,0 +1,13 @@
+/**@CopyRights Reserved to TescoBank pvt ltd. We Should not disclose this information to others
+ * Otherwise Terms & Conditions will apply
+ */
+package com.tesco.enquiry.service;
+
+/**
+ * @author VijayKumar at Aug 21, 2023
+ *
+ * 
+ */
+public interface ICreditLmitEnquiryService {
+
+}
