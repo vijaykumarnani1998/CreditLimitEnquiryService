c52f7efc5b7dcc68960dcacea0cbcf27bc581e13
vijayKumar <you@example.com>
2023-08-21 16:27:37 +0530

    First Commit

---
+++ CreditLimitEnquiryService/src/test/java/com/tesco/enquiry/CreditLimitEnquiryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tesco.enquiry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditLimitEnquiryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
